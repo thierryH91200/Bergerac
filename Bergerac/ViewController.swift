dad1ba3a844f0d4298a03015efa66d2e6a976ee2
thierry <thierry.hentic@gmail.com>
2022-04-19 10:41:29 +0200

    Initial Commit

---
+++ Bergerac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bergerac
+//
+//  Created by thierryH24 on 19/04/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
